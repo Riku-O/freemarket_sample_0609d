3bc73a211f694bfbfa5ae82264f5ac05dc4632e2
stevenvenvenven <take.19940627@gmail.com>
2019-06-30 14:29:56 +0900

    ・gemにunicornを追加 ・unicorn.rbファイルを作成 ・unicorn.rbファイルの中身を記述

---
+++ config/unicorn.rb
@@ -0,0 +1,52 @@
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+app_path = File.expand_path('../../', __FILE__ )
+
+#アプリケーションサーバの性能を決定する、workerの数を決めている
+worker_processes 1
+
+#アプリケーションに設置されているディレクトリを指定する
+working_directory app_path
+
+#Unicornの起動に必要なファイルの設置場所を指定
+pid "#{app_path}/tmp/pids/unicorn.pid"
+
+#ポート番号を指定
+listen 3000
+
+#エラーログを記録するファイルを作成する
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+
+#通常のログを記録するファイルを作成する
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+#Railsアプリからの応答を待つ上限時間を設定する
+timeout 60
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+run_once = true
+
+before_fork do |server, worker|
+  defined? (ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
+
+  if run_once
+    run_once = false
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid)
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
+    end
+  end
+end
+
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end
