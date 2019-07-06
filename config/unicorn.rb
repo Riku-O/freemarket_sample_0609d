811363b407821d95002529f105ef5bff6b32acd3
stevenvenvenven <take.19940627@gmail.com>
2019-07-07 00:19:55 +0900

    ・記述漏れを追記

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -37,7 +37,7 @@ before_fork do |server, worker|
   end
 
   old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid)
+  if File.exist?(old_pid) && server.pid != old_pid
     begin
       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
       Process.kill(sig, File.read(old_pid).to_i)
