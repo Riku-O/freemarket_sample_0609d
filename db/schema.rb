c88141e84e582759871357f09a4ca3a89e84beb6
stevenvenvenven <take.19940627@gmail.com>
2019-07-21 19:24:02 +0900

    ・itemsテーブルに外部キー制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_094900) do
+ActiveRecord::Schema.define(version: 2019_07_21_101725) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "path", null: false
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 2019_07_21_094900) do
     t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "user_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -71,5 +75,7 @@ ActiveRecord::Schema.define(version: 2019_07_21_094900) do
   end
 
   add_foreign_key "item_images", "items"
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
   add_foreign_key "user_informations", "users"
 end
