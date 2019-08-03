c00403d5c4e7414be2db433746e0dd4a68b4fd57
stevenvenvenven <take.19940627@gmail.com>
2019-08-03 17:36:38 +0900

    ・ブランドテーブルの作成 ・アソシエーションはbrandテーブル部分のみの記述 　→itemモデルは一覧表示のブランチで記述する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_060846) do
+ActiveRecord::Schema.define(version: 2019_08_03_083140) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ancestry"
@@ -43,6 +49,8 @@ ActiveRecord::Schema.define(version: 2019_07_28_060846) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
+    t.bigint "brand_id"
+    t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
@@ -76,6 +84,7 @@ ActiveRecord::Schema.define(version: 2019_07_28_060846) do
   end
 
   add_foreign_key "item_images", "items"
+  add_foreign_key "items", "brands"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
   add_foreign_key "user_informations", "users"
