2ca017fe257e563a75edf415013200f57fcf8182
stevenvenvenven <take.19940627@gmail.com>
2019-08-04 18:41:42 +0900

    ・一連のアクションを作成 ・before_actionでitemの情報を取得する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.fetch_items
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
