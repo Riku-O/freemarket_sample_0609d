cd026b7622ffd0318532985ee0744f4b4382e291
hirokihanabusa <clouscolsunset@yahoo.co.jp>
2019-08-04 15:17:21 +0900

    initial_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'items' => 'items#index'
-<<<<<<< HEAD
-  get 'sells' => 'sells#show'
-
-=======
   get 'mypage' => 'mypage#index'
->>>>>>> origin
 end
