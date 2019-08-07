150e42ec998249fba25be1034cb2a8a705ba60d5
stevenvenvenven <take.19940627@gmail.com>
2019-08-07 15:26:51 +0900

    ・画像保存時に規定以上の枚数を保存できないようにvalidationを追加

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -2,4 +2,13 @@ class ItemImage < ApplicationRecord
   belongs_to :item
   # mount_uploader :image, ImageUploader
   validates :image, presence: true
+  validate :check_number_of_images
+
+  # imagesの数を制限するvalidation、validationがfalseしたときの挙動はどうしようか
+  def check_number_of_images
+    if item.images.count > 5
+      false
+      errors.add(:item_images, "画像は５枚までしか保存できません。")
+    end
+  end
 end
