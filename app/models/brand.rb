7339dec89c7f05cc90a5c65ecc332bd1c0f3d467
stevenvenvenven <take.19940627@gmail.com>
2019-08-08 18:37:23 +0900

    ・ブランド登録のメソッドを書き換え 　save→update（リソースの作成ではなく書き換えのため）

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -6,7 +6,7 @@ class Brand < ApplicationRecord
   def self.save_brand(item, brand_name)
     if brand_name.present?
       brand = Brand.find_or_create_by!(name: brand_name)
-      item.save(brand_id: brand.id)
+      item.update!(brand_id: brand.id)
     end
   end
 end
