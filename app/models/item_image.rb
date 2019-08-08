d468aa8ac342d698212e1e6a57f3c82618a4bdb2
stevenvenvenven <take.19940627@gmail.com>
2019-08-08 15:32:16 +0900

    画像保存を１０枚までできるようにvalidationの記述を修正

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -6,9 +6,9 @@ class ItemImage < ApplicationRecord
 
   # imagesの数を制限するvalidation、validationがfalseしたときの挙動はどうしようか
   def check_number_of_images
-    if item.images.count > 5
+    if item.images.count > 11
       false
-      errors.add(:item_images, "画像は５枚までしか保存できません。")
+      errors.add(:item_images, "画像は１０枚までしか保存できません。")
     end
   end
 end
