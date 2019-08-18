f7a6e202c6278ac020ea1c8dba04b50dd2c8e685
hirokihanabusa <clouscolsunset@yahoo.co.jp>
2019-08-18 15:14:37 +0900

    validates記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
   has_many :deals
   has_many :buyer_deals, class_name: 'Deal', foreign_key: 'buyer_id'
   has_many :seller_deals, class_name: 'Deal', foreign_key: 'seller_id'
+  has_one :user_information
+  validates :nickname, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, length: { in: 7..128 }, confirmation: true
+  validates :encrypted_password_confirmation, length: { in: 7..128 }, confirmation: true
 end
