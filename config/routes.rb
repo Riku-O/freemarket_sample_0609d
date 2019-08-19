1ec27145b9957af5a08bed2823f685ae4267bc22
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2019-08-20 03:39:25 +0900

    会員登録完了マークアップ完了

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     get "users/sign_up/sms_confirmation" => "devise/registrations#sms_confirmation" # 電話番号認証ページ
     get "users/sign_up/address" => "devise/registrations#address" # 発送元・お届け先住所入力ページ
     get "users/sign_up/credit_card" => "devise/registrations#credit_card" # 支払い方法入力ページ
+    get "users/sign_up/done" => "devise/registrations#done" # 会員登録完了ページ
   end
   devise_for :users
   root 'items#index'
