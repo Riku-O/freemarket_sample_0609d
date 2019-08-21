4d0b76a4a450d4775163db6378a601d24fd351e1
hirokihanabusa <clouscolsunset@yahoo.co.jp>
2019-08-21 22:35:07 +0900

    コメントアウト解除

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
   has_many :seller_deals, class_name: 'Deal', foreign_key: 'seller_id'
   has_one :user_information
   has_one :address, dependent: :destroy
-  # validates :nickname, presence: true, length: { maximum: 20 }
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email, {presence: true, , format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
-  # validates :encrypted_password, length: { in: 7..128 }, confirmation: true
-  # validates :encrypted_password_confirmation, length: { in: 7..128 }, confirmation: true
+  validates :nickname, presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, , format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+  validates :encrypted_password, length: { in: 7..128 }, confirmation: true
+  validates :encrypted_password_confirmation, length: { in: 7..128 }, confirmation: true
 end
