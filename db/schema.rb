18e0f36ef7540e14475b91b8ef01f310ecdeb189
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2019-08-24 01:26:41 +0900

    userテーブルにomniauth用のカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_043541) do
+ActiveRecord::Schema.define(version: 2019_08_23_154927) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postcode", null: false
@@ -104,6 +104,8 @@ ActiveRecord::Schema.define(version: 2019_08_18_043541) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
