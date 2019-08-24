b162ef87b795c9f15a109dbfef88f2f021cd1ea0
stevenvenvenven <take.19940627@gmail.com>
2019-08-24 22:19:48 +0900

    ・不要な記述を削除

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,10 +2,5 @@ class Deal < ApplicationRecord
   belongs_to :item
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
-
-  after_destroy :deal_destroy_action
-
-  def deal_destroy_action
-    puts "deal_destroy"
-  end
+  
 end
