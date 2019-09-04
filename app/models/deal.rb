afd69e344aaa669a31397fcd328700d2579a9442
stevenvenvenven <take.19940627@gmail.com>
2019-09-04 17:07:03 +0900

    取引中のメッセージのモデル作成と、アソシエーションの記述

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,6 +1,12 @@
 class Deal < ApplicationRecord
+  has_many :deal_messages, dependent: :destroy
   belongs_to :item
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
-  
+
+  enum status: {unfinished: 0, finished: 1}
+
+  def self.fetch_deal(deal_id)
+    Deal.find_by(deal_id)
+  end
 end
