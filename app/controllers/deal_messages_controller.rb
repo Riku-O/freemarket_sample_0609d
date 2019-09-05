e2ba7d0f3f2dfafacbddfe33a902ab6d458642c6
stevenvenvenven <take.19940627@gmail.com>
2019-09-05 17:32:23 +0900

    メッセージの投稿、保存、一覧表示のメソッドを記述

--- app/controllers/deal_messages_controller.rb
+++ app/controllers/deal_messages_controller.rb
@@ -1,9 +1,29 @@
 class DealMessagesController < ApplicationController
-  def new
+  before_action :set_deal, only: [:new, :create]
+  # before_action :authenticate_user!
 
+  def index
+    @deal_messages = @deal.deal_messages.includes(:user)
+  end
+
+  def new
   end
 
   def create
+    DealMessage.create!(
+                   message: message_params[:message],
+                   deal_id: message_params[:deal_id],
+                   user_id: current_user.id
+    )
+    redirect_to @deal
+  end
 
+  private
+  def set_deal
+    @deal = Deal.find(params[:deal_id])
+  end
+
+  def message_params
+    params.permit(:message, :deal_id)
   end
 end
--- app/models/deal_message.rb
+++ app/models/deal_message.rb
@@ -1,4 +1,8 @@
 class DealMessage < ApplicationRecord
   belongs_to :deal
   belongs_to :user
+  validates :user_id, presence: true
+  validates :message, presence: true
+  validates :user_id, presence: true
+
 end
