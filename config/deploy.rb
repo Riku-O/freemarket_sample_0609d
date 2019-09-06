ad1dadd6c91513f8f5c92c3171defceb616b0e7e
stevenvenvenven <take.19940627@gmail.com>
2019-09-06 22:21:13 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   RAILS_MASTER_KEY: ENV["RAILS_MASTER_KEY"],
+  SECRET_KEY_BASE: ENV["SECRET_KEY_BASE"],
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
