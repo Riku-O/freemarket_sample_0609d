ac32d527ad38ecca619855b842b4d820632476ac
AtsushiKuwabara <at_kuwabara@icloud.com>
2019-09-07 18:45:05 +0900

    商品詳細表示微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,6 +11,7 @@ class ItemsController < ApplicationController
   def show
     #TODO:is_hosted_by?メソッドを使って、出品者以外が閲覧した際に編集ボタンが表示されないようにする。
     @item = Item.fetch_item(params[:id])
+    @user = User.find(params[:id])
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: :show
 
   def show
+    
   end
 
   def edit
