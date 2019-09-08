3f9d8d5ccff297c55e072d62d8897d1c2d92b300
stevenvenvenven <take.19940627@gmail.com>
2019-09-08 11:55:10 +0900

    ログアウト後のリダイレクト先を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(resource)
+    '/'
+  end
+
+  private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name,
                                                        :last_name_kana, :first_name_kana, :birth_year,
