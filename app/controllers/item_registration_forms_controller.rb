f75ae1b86d0c40ac23d22f1b2fdc742ce208a48f
stevenvenvenven <take.19940627@gmail.com>
2019-09-09 16:03:52 +0900

    一旦コミット

--- app/controllers/item_registration_forms_controller.rb
+++ app/controllers/item_registration_forms_controller.rb
@@ -14,6 +14,25 @@ class ItemRegistrationFormsController < ApplicationController
     end
   end
 
+  def edit
+    @item_registration_form = ItemRegistrationForm.new(name: set_item.name, condition_id: set_item.condition_id,
+                                                       post_burden_id: set_item.post_burden_id, source_area_id: set_item.source_area_id,
+                                                       shipping_date_id: set_item.shipping_date_id,
+                                                       price: set_item.price, description: set_item.description,
+                                                       category_id: set_item.category_id, user_id: set_item.user_id,
+                                                       item_images_attributes: {image: set_item.item_images.first.image}
+                                                       )
+    if @item_registration_form.update
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def update
+
+  end
+
   private
   # TODO:アイテムのブランドは後日実装
   def item_registration_params
@@ -23,5 +42,9 @@ class ItemRegistrationFormsController < ApplicationController
                                           :category_id,
                                           item_images_attributes: [:image])
                                           .merge(user_id: current_user.id)
+  end
+
+  def set_item
+    Item.find(params[:id])
   end
 end
