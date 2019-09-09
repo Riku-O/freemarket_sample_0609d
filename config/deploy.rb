81d1f5c976e6ec1dea344667c475b97b434ccb12
stevenvenvenven <take.19940627@gmail.com>
2019-09-10 00:08:11 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,13 +35,13 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload master.key'
+  desc 'upload credentials.yml.enc'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/master.key', "#{shared_path}/config/master.key")
+      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
     end
   end
   before :starting, 'deploy:upload'
