e98896621423ca07dce5b62124fffc32de06eb8d
stevenvenvenven <take.19940627@gmail.com>
2019-09-09 23:25:39 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,15 +35,15 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  # desc 'upload credentials.yml.enc'
-  # task :upload do
-  #   on roles(:app) do |host|
-  #     if test "[ ! -d #{shared_path}/config ]"
-  #       execute "mkdir -p #{shared_path}/config"
-  #     end
-  #     upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
-  #   end
-  # end
+  desc 'upload credentials.yml.enc'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
+    end
+  end
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
