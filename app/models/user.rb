07406c4c9c0f674c724210c10e78b1f8c9d3883a
hirokihanabusa <clouscolsunset@yahoo.co.jp>
2019-09-14 17:00:32 +0900

    削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # has_many :items
-  # has_many :deals
-  # has_many :buyer_deals, class_name: 'Deal', foreign_key: 'buyer_id'
-  # has_many :seller_deals, class_name: 'Deal', foreign_key: 'seller_id'
-  # has_one :user_information
-  # has_one :address, dependent: :destroy
-  # has_one :user_information, dependent: :destroyvalidates :nickname, presence: true, length: { maximum: 50 }
-  # validates :nickname, {presence: true, length: { maximum: 20 }}
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
-  # validates :encrypted_password, {presence: true, length: { in: 7..128 }, format: { with: /\A[a-z0-9]+\z/i } }
-
 end
