62685d20b19f06fae841e216b09df3bbed5cf517
hirokihanabusa <clouscolsunset@yahoo.co.jp>
2019-09-14 16:56:58 +0900

    initial_commit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(user_params)
+  end
 
   # GET /resource/edit
   # def edit
