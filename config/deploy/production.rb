ab33d29b753d68266f91115a1fa6fc482cd68640
stevenvenvenven <take.19940627@gmail.com>
2019-09-14 14:37:11 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,8 @@ server "52.198.3.9", user: "ec2-user", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
