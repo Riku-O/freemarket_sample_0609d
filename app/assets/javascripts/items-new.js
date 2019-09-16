b0460d876639b29f595f6468667927c9ff7e5e4b
stevenvenvenven <take.19940627@gmail.com>
2019-09-16 20:14:46 +0900

    修正

--- app/assets/javascripts/items-new.js
+++ app/assets/javascripts/items-new.js
@@ -9,6 +9,7 @@ $(function () {
         var profit = set_price * 0.9;
         return profit
     }
+    //金額を入力したら計算するメソッド
     $('.input-default').change(function () {
         $('.sell-fee').text(calculate_fee());
         $('.sell-profit').text(calculate_profit());
