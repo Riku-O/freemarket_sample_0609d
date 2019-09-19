c3ab48751db71ed9cddc8e79c2d3dbc015108b66
stevenvenvenven <take.19940627@gmail.com>
2019-09-19 16:58:44 +0900

    ブランド入力用のテキストボックスを追加する処理を追加

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,14 +1,27 @@
 $(function () {
     function buildSelect(){
         var html = `
-            <div class="select-wrap">
+            <div class="select-wrap" id="subcategory-select">
               <i class="fa fa-angle-down"></i>
               <select class="select-default"></select>
               </div>`
-        $('#appending_form').append(html)
+        $('#appending_form').append(html);
+        $('#subcategory-select').append(buildBrand());
     }
     function buildSelectOption(){
 
+    }
+    function buildBrand(){
+        var html =
+            `<div class="form-group brand-text">
+              <label>
+                ブランド
+                <span class="form-arbitrary">任意</span>
+                </label>
+              <div>
+                <input class="input-default" placeholder="例）シャネル" value="" name="brand">
+                </div>  
+              </div>`
     }
 
     $().on('change', function () {
